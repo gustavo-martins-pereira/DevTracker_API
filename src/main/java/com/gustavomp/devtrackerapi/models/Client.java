979e0c30fa792cf30b9e22e570e2aa7036a559f7
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2026-09-06 11:22:52 -0300

    ADD - Client Entity

---
+++ src/main/java/com/gustavomp/devtrackerapi/models/Client.java
@@ -0,0 +1,28 @@
+package com.gustavomp.devtrackerapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private Boolean active;
+
+}
