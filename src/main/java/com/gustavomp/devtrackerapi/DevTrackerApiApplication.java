db02af89b0082679c92c2637515dd62d4df049c2
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2026-09-06 11:15:32 -0300

    INI - Project Initialized

---
+++ src/main/java/com/gustavomp/devtrackerapi/DevTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomp.devtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gustavomp/devtrackerapi/DevTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomp.devtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
